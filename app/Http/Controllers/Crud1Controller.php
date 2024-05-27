4eeb97083c4cc83ef0da28827aeffe0468ea0ff7
Vladimir Gordeev <WorkStationGuard@gmail.com>
2024-05-27 23:04:39 +0300

    init + crud1 vol.1

---
+++ app/Http/Controllers/Crud1Controller.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CRUD1;
+use Illuminate\Http\Request;
+
+class Crud1Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CRUD1 $cRUD1)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CRUD1 $cRUD1)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CRUD1 $cRUD1)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CRUD1 $cRUD1)
+    {
+        //
+    }
+}
